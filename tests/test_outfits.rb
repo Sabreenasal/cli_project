71c48d877a06829ca642d12c39a066acd2a3823f
Sabreena Salomon <salomonbree@gmail.com>
2024-11-11 21:45:25 +0000

    Edited test file

--- tests/test_outfits.rb
+++ tests/test_outfits.rb
@@ -1,10 +1,9 @@
 require 'minitest/autorun'
 require './outfits'
 
-class TestOutfits < MiniTest :: Test
+class TestOutfits < Minitest::Test
   def test_outfits
-   outfits = Outfits.new
-   outfits.to_s = "#{@color} #{@garment}"
-   assert_equal "#{@color} #{@garment}", outfits.to_s, "to_s method not found"
+   outfit = Outfits.new("pink", "pants")
+   assert_equal "pink pants", outfit.to_s, "to_s method not found"
   end
 end

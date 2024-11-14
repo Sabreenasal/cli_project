7da93b2b204ac42f015863571dda9004fac67c7b
Sabreena Salomon <salomonbree@gmail.com>
2024-11-14 03:45:26 +0000

    corrected algorithm for remove_garment method

--- closet.rb
+++ closet.rb
@@ -1,58 +1,54 @@
 require_relative "outfits"
 
 class Closet
-  def initialize 
+  def initialize
     @outfits = []
   end
 
-
- def run
-  loop do
-   puts "\n"
-   puts "Welcome to your closet!"
-   puts "1. List all garments"
-   puts "2. Take out a garment"
-   puts "3. Add a garment"
-   puts "4. Exit"
-   print "Choose an option:"
-   puts "\n"
-   option = gets.chomp.to_i
-
-  
-   case option 
-    when 1
-      list_garments
-    when 2
-      remove_garment
-    when 3
-      add_garment
-    when 4 
+  def run
+    loop do
       puts "\n"
-      puts "See ya later!"
-    exit 
-    else
-    puts "\n"
-    puts "Invalid option. Please choose another option."
-  run
-  end 
+      puts "Welcome to your closet!"
+      puts "1. List all garments"
+      puts "2. Take out a garment"
+      puts "3. Add a garment"
+      puts "4. Exit"
+      print "Choose an option:"
+      puts "\n"
+      option = gets.chomp.to_i
+
+      case option
+      when 1
+        list_garments
+      when 2
+        remove_garment
+      when 3
+        add_garment
+      when 4
+        puts "\n"
+        puts "See ya later!"
+        exit
+      else
+        puts "\n"
+        puts "Invalid option. Please choose another option."
+        run
+      end
+    end
   end
-end
 end
 
 private
 
 def list_garments
-   puts "\n"
-   puts "Garments in the closet!:"
-   puts "\n"
-   @outfits.each do |outfits|
-   puts outfits
+  puts "\n"
+  puts "Garments in the closet!:"
+  puts "\n"
+  @outfits.each do |outfits|
+    puts outfits
   end
-  if
-   @outfits.empty? 
-   puts "The closet is empty. Please add a garment or exit."
+  if @outfits.empty?
+    puts "The closet is empty. Please add a garment or exit."
   end
-
 end
 
 def add_garment
@@ -62,28 +58,46 @@ def add_garment
   puts "\n"
   puts "What kind of garment is this?"
   garment = gets.chomp
-  @outfits << Outfits.new(color,garment)
+  @outfits << Outfits.new(color, garment)
   puts "\n"
   puts "Garment has been added!"
 end
 
 def remove_garment
-  if
-    @outfits.empty? 
-    puts "The closet is empty. Please add a garment or exit."
-    run
+  puts "\n"
+  if @outfits.empty?
+    puts "You currently do not have any outfits in your closet"
+  else
+    puts "What is the oufit you want to wear today? Example: blue top"
+    article_of_clothing = gets.chomp
+    closet_search = @outfits.find { |outfit|
+      if outfit.to_s.downcase == article_of_clothing
+        @outfits.delete(outfit)
+        puts "You took out: #{article_of_clothing}! You look amazing"
+      elsif outfit != article_of_clothing
+        puts "That isn't in the closet. Please type something from the list"
+      end
+    }
   end
-  puts "\n"
-  puts " What is the oufit you want to wear today? Example: blue top"
-  outfits = gets.chomp
-  outfit = @outfits.find { |outfit| if outfit.to_s.downcase == outfits
-  @outfits.delete(outfit)
-  end}
-if outfits != outfit
-  puts "\n"
-  puts "That isn't in the closet. Please type something from the list "
- remove_garment
 end
-  puts "\n"
-  puts "You look amazing!"
-end
+
+# def remove_garment
+#   puts "\n"
+#   puts "What is the oufit you want to wear today? Example: blue top"
+#   outfits = gets.chomp
+#   outfit = @outfits.find { |outfit| if outfit.to_s.downcase == outfits
+#   @outfits.delete(outfit)
+#   end}
+#   if
+#     @outfits.empty?
+#     puts "\n"
+#     puts "The closet is empty. Please add a garment or exit."
+#   end
+# if outfits != outfit
+#   puts "\n"
+#   puts "That isn't in the closet. Please type something from the list "
+#  remove_garment
+# end
+#   puts "\n"
+#   puts "You look amazing!"
+# end
